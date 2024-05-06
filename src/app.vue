9dcefb4839b5210ed5ecccb76f45715510b3da46
Crew <113601819+crewcik@users.noreply.github.com>
2024-05-06 09:43:26 +0300

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <GithubProfile />
+  </div>
+</template>
+
+<script>
+import GithubProfile from './components/GithubProfile.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GithubProfile
+  }
+};
+</script>
